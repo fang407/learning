08f382fe2464946421083c44ab5d6dd259e8f3ef
fang <fang9761@gmail.com>
2025-12-01 21:56:12 +0800

    add transaction class

--- oes_core/models.py
+++ oes_core/models.py
@@ -44,3 +44,33 @@ class Product:
             'price': self.price,
             'current_stock': self.current_stock
         }
+
+@dataclass
+class Transaction:
+    """
+    model for recording all transaction types and quantities.
+    """
+    # TYPE CONSTANTS
+    TYPE_INBOUND = "INBOUND"
+    TYPE_OUTBOUND = "OUTBOUND"
+    TYPE_ADJUSTMENT = "ADJUSTMENT"
+
+    transaction_id: str = field(default_factory=lambda: str(uuid.uuid4()), init=False)
+
+    # associated attribute
+    product_id: str
+
+    # core attribute
+    quantity_change: int
+    transaction_type: str
+    # metadata
+    timestamp: datetime = field(default_factory=datetime.now, init=False)
+
+    def __post_init__(self):
+        if self.transaction_type not in (self.TYPE_INBOUND, self.TYPE_OUTBOUND, self.TYPE_ADJUSTMENT):
+            raise ValueError(f"Invalid transaction type:{self.transaction_type}")
+        if self.transaction_type == self.TYPE_INBOUND and self.quantity_change <= 0:
+            raise ValueError("INBOUND transaction must have a positive quantity change.")
+        if self.transaction_type == self.TYPE_OUTBOUND and self.quantity_change >= 0:
+            raise ValueError("OUTBOUND transaction must have a negative quantity change.")
+
