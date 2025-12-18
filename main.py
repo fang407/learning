7d06e3297e9d992f94c3fc240f8fd557ac89007c
fang <fang9761@gmail.com>
2025-12-18 23:21:42 +0800

    goodnight

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # This is a sample Python script.
 import time
 import os
+import json
 import logging
 logger = logging.getLogger(__name__)
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello! {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 def get_something_good():
-    print("Get locky today!")
+    print("Get lucky today!")
+    logger.info("Typo happens, but no worries.")
+
     return True
 
 # Press the green button in the gutter to run the script.
@@ -21,10 +22,10 @@ if __name__ == '__main__':
     logger.info("Im fine thank you!!! Good practice to print everything out for debug HAHA")
     logger.warning("Warning in your area")
     print("Actually no warning shown in the console, what happened??")
-    logger.info("Developer debugging...")
+    logger.warning("Developer debugging...")
 
     good_day = get_something_good()
     good_night = get_something_good()
 
     assert good_day == good_night
-    assert 100 == 100
+    assert 100 != 101
