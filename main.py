25e2289b45e82703d5004d29a87fa8cb70ef12b3
fang <fang9761@gmail.com>
2025-12-25 20:57:20 +0800

    hmm

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ def get_something_good():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     logger.warning("Developer debugging...Take naps now!")
-    logger.info("Actually, this is to check if we can print log in console.")
+    logger.error("Actually, this is to check if we can print log in console.")
+    logger.info("Do we need to set logger level in main.py? im not too sure!")
 
     good_day = get_something_good()
-    good_night = get_something_good() and False
+    good_night = get_something_good()
 
-    assert good_day != good_night
+    assert good_day == good_night
     logging.info("Now im worried...")
 
